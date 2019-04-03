67ed2a4fe28955dff4b18a60e7768975901032dc
Adrian <210171@edu.p.lodz.pl>
2019-04-03 17:54:10 +0200

    Basic repositories for useers + few fixes for abstract cru repository

--- src/main/java/pl/lodz/p/it/ssbd2019/ssbd03/repository/AbstractCruRepository.java
+++ src/main/java/pl/lodz/p/it/ssbd2019/ssbd03/repository/AbstractCruRepository.java
@@ -2,13 +2,14 @@ package pl.lodz.p.it.ssbd2019.ssbd03.repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 import java.util.Optional;
 
-public abstract class AbstractCruRepository<T,ID> implements CruRepository<T, ID> {
+public abstract class AbstractCruRepository<T, ID> implements CruRepository<T, ID> {
 
     protected abstract EntityManager getEntityManager();
-    protected abstract String getTableName();
     protected abstract Class<T> getTypeParameterClass();
 
     @Override
@@ -41,9 +42,29 @@ public abstract class AbstractCruRepository<T,ID> implements CruRepository<T, ID
 
     @Override
     public List<T> findAll() {
-        String select = String.format("SELECT * FROM %s", getTableName());
-        Query query = getEntityManager().createQuery(select);
-        return query.getResultList();
+        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(getTypeParameterClass()));
+        return getEntityManager().createQuery(cq).getResultList();
+    }
+
+    protected TypedQuery<T> createNamedQuery(String queryName) {
+        return this.createNamedQuery(queryName, getTypeParameterClass());
+    }
+
+    protected TypedQuery<T> createNamedQuery(String name, Class<T> resultClass) {
+        return getEntityManager().createNamedQuery(name, resultClass);
+    }
+
+    protected int executeUpdateQuery(String query) {
+        return getEntityManager().createQuery(query).executeUpdate();
+    }
+
+    protected int executeUpdateNamedQuery(String name) {
+        return getEntityManager().createNamedQuery(name).executeUpdate();
+    }
+
+    protected Query createQuery(String query) {
+        return getEntityManager().createQuery(query);
     }
 
 }
