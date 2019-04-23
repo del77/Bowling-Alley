7aecb1fcdbfd36be01dd2da9acd15c28472459b0
Adrian <210171@edu.p.lodz.pl>
2019-04-23 12:02:14 +0200

    SSBD201903-76 configured static resources servlet

--- src/main/java/pl/lodz/p/it/ssbd2019/ssbd03/accountsmodule/service/RegistrationService.java
+++ src/main/java/pl/lodz/p/it/ssbd2019/ssbd03/accountsmodule/service/RegistrationService.java
@@ -6,6 +6,14 @@ import pl.lodz.p.it.ssbd2019.ssbd03.exceptions.EntityRetrievelException;
 import pl.lodz.p.it.ssbd2019.ssbd03.exceptions.RegistrationProcessException;
 
 public interface RegistrationService {
+
+    /**
+     * Wiąże encje oraz tworzy konto użytkownika o danych zawartych w podanych obiektach.
+     * @param account Obiekt klasy Account, który ma zostać dodany do bazy danych.
+     * @param user Obiekt klasy user, który ma zostać dodany do bazy danych.
+     * @throws RegistrationProcessException W przypadku niespodziewanego błędu w trakcie rejestracji.
+     * @throws EntityRetrievelException W przypadku, gdy nie jest w stanie pozyskać encji AccessLevel.
+     */
     void registerAccount(Account account, User user) throws RegistrationProcessException, EntityRetrievelException;
 
     /**
