812fb5f0fc43cdb2e24761532d9109ee8490bb09
sopeal <micsobczyk@gmail.com>
2019-06-07 18:29:01 +0200

    SSBD201903-214 added view, controller, service, repository, named query

---
+++ src/main/java/pl/lodz/p/it/ssbd2019/ssbd03/mor/web/dto/ReservationDto.java
@@ -0,0 +1,37 @@
+package pl.lodz.p.it.ssbd2019.ssbd03.mor.web.dto;
+
+import lombok.Data;
+import pl.lodz.p.it.ssbd2019.ssbd03.validators.ValidReservationDates;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.FormParam;
+import java.sql.Timestamp;
+
+@Data
+@ValidReservationDates
+public class ReservationDto {
+
+    @NotNull(message = "{validate.startDateIsNull}")
+    @FutureOrPresent(message = "{validate.startDateBeforePresent}")
+    @FormParam("startDate")
+    private Timestamp startDate;
+
+    @NotNull(message = "{validate.endDateIsNull}")
+    @Future(message = "{validate.startDateBeforePresent}")
+    @FormParam("endDate")
+    private Timestamp endDate;
+
+    @Min(value = 1, message = "{validate.playersCountBelow1}")
+    @FormParam("playersCount")
+    private int playersCount;
+
+    @FormParam("active")
+    private boolean active;
+
+    @FormParam("alleyNumber")
+    private int alleyNumber;
+
+}
