2c0c127c755cc2dd3823e374268a9120fd483b2f
del <xdelpl@gmail.com>
2019-06-26 21:07:38 +0200

    update entities

--- src/main/java/pl/lodz/p/it/ssbd2019/ssbd03/entities/ConfirmationToken.java
+++ src/main/java/pl/lodz/p/it/ssbd2019/ssbd03/entities/ConfirmationToken.java
@@ -34,10 +34,7 @@ public class ConfirmationToken {
     private String token;
 
     @NotNull
-    @OneToOne(
-            cascade = CascadeType.MERGE,
-            fetch = FetchType.EAGER
-    )
+    @OneToOne
     @JoinColumn(name = "user_id",
             updatable = false,
             nullable = false,
