626d65dcdc59d319e77a9291a641e2345d871f5f
Dariusz Syncerek <dsyncerek@outlook.com>
2019-06-26 23:47:18 +0200

    SSBD201903-253 Add catching ConstraintViolationException

--- src/main/java/pl/lodz/p/it/ssbd2019/ssbd03/mor/repository/CommentRepositoryLocalImpl.java
+++ src/main/java/pl/lodz/p/it/ssbd2019/ssbd03/mor/repository/CommentRepositoryLocalImpl.java
@@ -3,6 +3,7 @@ package pl.lodz.p.it.ssbd2019.ssbd03.mor.repository;
 import pl.lodz.p.it.ssbd2019.ssbd03.entities.Comment;
 import pl.lodz.p.it.ssbd2019.ssbd03.exceptions.entity.CommentOptimisticLockException;
 import pl.lodz.p.it.ssbd2019.ssbd03.exceptions.entity.DataAccessException;
+import pl.lodz.p.it.ssbd2019.ssbd03.exceptions.entity.DatabaseConstraintViolationException;
 import pl.lodz.p.it.ssbd2019.ssbd03.repository.AbstractCruRepository;
 import pl.lodz.p.it.ssbd2019.ssbd03.utils.roles.MorRoles;
 
@@ -14,6 +15,7 @@ import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.OptimisticLockException;
 import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolationException;
 
 @Stateless(name = "MORCommentRepository")
 @TransactionAttribute(TransactionAttributeType.MANDATORY)
@@ -40,6 +42,8 @@ public class CommentRepositoryLocalImpl extends AbstractCruRepository<Comment, L
             super.edit(comment);
         } catch (OptimisticLockException e) {
             throw new CommentOptimisticLockException(e);
+        } catch (ConstraintViolationException e) {
+            throw new DatabaseConstraintViolationException(e);
         }
     }
 }
